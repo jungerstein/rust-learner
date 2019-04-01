f2456951ff5174d6f4568329bb191ae7d034c371
jungerstein <BeatlesFan1987@gmail.com>
2019-04-01 15:15:28 +0800

    Add another referrence -- by subscripts.

--- proj_wo_cargo/datatypes/main.rs
+++ proj_wo_cargo/datatypes/main.rs
@@ -3,4 +3,6 @@ fn main()
   let gougu = (3, 4, 5); // Packing. 
   let (gou, gu, xian) = gougu; // Unpacking. 
   println!("Gou {}, gu {}, xian {}", gou, gu, xian); 
+  // Also referred as 
+  println!("{}^2 + {}^2 == {}^2", gougu.0, gougu.1, gougu.2); 
 }
