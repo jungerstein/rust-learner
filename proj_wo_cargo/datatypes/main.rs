9fec7065aabee84f518c327ffca252463c457b85
jungerstein <BeatlesFan1987@gmail.com>
2019-04-01 15:18:44 +0800

    Added array grammar.

--- proj_wo_cargo/datatypes/main.rs
+++ proj_wo_cargo/datatypes/main.rs
@@ -5,4 +5,7 @@ fn main()
   println!("Gou {}, gu {}, xian {}", gou, gu, xian); 
   // Also referred as 
   println!("{}^2 + {}^2 == {}^2", gougu.0, gougu.1, gougu.2); 
+
+  let shuzu :[i32; 10] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; 
+  println!("Cong 0 kaishi shu, biru shuzu[7] = {}", shuzu[7]); 
 }
